2eae4d91792e336929cb68fc579fdfc48a5bb95d
Roshan <e1122413@u.nus.edu>
2025-08-21 15:22:37 +0800

    Level-1

--- src/main/java/Silvermoon.java
+++ src/main/java/Silvermoon.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class Silvermoon {
     private static final String NAME = "Silvermoon";
     private static final String LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
         greet();
-        exit();
+        run();
     }
 
     private static void greet() {
@@ -14,8 +16,27 @@ public class Silvermoon {
         System.out.println(LINE);
     }
 
-    private static void exit() {
-        System.out.println(" Bye. Hope to see you again soon!");
+    private static void run() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                exit();
+                break;
+            }
+            echo(input);
+        }
+    }
+
+    private static void echo(String msg) {
+        System.out.println(LINE);
+        System.out.println(" " + msg);
         System.out.println(LINE);
     }
+
+    private static void exit() {
+        System.out.println(LINE);
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
 }
+
