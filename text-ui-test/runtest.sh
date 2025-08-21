5f00982d542286bdcbb161819de4a935066a6737
Roshan <e1122413@u.nus.edu>
2025-08-21 18:49:00 +0800

    A-TextUiTesting

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,15 +12,15 @@ then
     rm ACTUAL.TXT
 fi
 
-# compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+# compile the code into the bin folder, terminate if error occurred
+if ! javac -Xlint:none -d ../bin ../src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+# run the program, feed commands from input.txt file and redirect the output to ACTUAL.TXT
+java -classpath ../bin Silvermoon < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
